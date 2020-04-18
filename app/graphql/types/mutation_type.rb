bb465fe4bc0eb3b91d00a03f115115e67027eced
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2020-04-18 20:48:03 +0530

    Feat: Update an author

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -13,5 +13,14 @@ module Types
     # end
 
     field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
+
+    field :update_author, Boolean, null: false, description: "Update an author" do
+      argument :author, Types::AuthorInputType, required: true
+    end
+
+    def update_author(author:)
+      existing = Author.where(id: author[:id]).first
+      existing&.update(author.to_h)
+    end
   end
 end
